3f8b643aa8b97713cd739547472e26b5f920a951
Anders Ericsson <998160+ericssonanders@users.noreply.github.com>
2024-09-16 14:03:56 +0200

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -8,7 +8,7 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
-	ip := os.Getenv("FRONTEND_HOST")
+	ip := os.Getenv("FRONTEND_SERVICE_HOST")
 	
 	if len(response) == 0 {
 		response = "Hello OpenShift for Developers!!"
